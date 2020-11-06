a522311063610dbc6e2317278ae9bbfbb2b04a79
Þorsteinn Sigurðsson <ths251@hi.is>
2020-11-06 16:42:39 +0000

    tweepy dresl

--- my_bot.py
+++ my_bot.py
@@ -6,3 +6,12 @@ Created on Fri Nov  6 16:14:47 2020
 """
 import tweepy
 print('yo')
+
+CONSUMER_KEY =''
+CONSUMER_SECRET = ''
+ACCESS_KEY =''
+ACCESS_SECRET=''
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
